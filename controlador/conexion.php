a41dceb25ce84194fb122e7a2a787101637f1cb2
Dahiana-moreno <dahian1507becerra@gmail.com>
2024-07-04 20:16:57 -0500

    files env

--- controlador/conexion.php
+++ controlador/conexion.php
@@ -1,6 +1,14 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php'; // Asegúrate de que la ruta es correcta
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
 
-    $conexion = mysqli_connect('localhost','root','','eventos');
+$dbHost = $_ENV['DB_HOST'];
+$dbName = $_ENV['DB_NAME'];
+$dbUser = $_ENV['DB_USER'];
+$dbPass = $_ENV['DB_PASSWORD'];
+
+    $conexion = mysqli_connect($dbHost, $dbUser,$dbPass, $dbName);
     $tildes = $conexion->query("SET NAMES 'utf8'");
     if(mysqli_connect_error()){
         die('No se puede conectar a la base de datos'.mysqli_connect_error());

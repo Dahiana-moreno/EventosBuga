3fa076dec23ff08697b07f2e726f429c99540ae9
Dahiana-moreno <dahian1507becerra@gmail.com>
2024-07-04 20:58:41 -0500

    index

--- controlador/conexion.php
+++ controlador/conexion.php
@@ -7,8 +7,9 @@ $dbHost = $_ENV['DB_HOST'];
 $dbName = $_ENV['DB_NAME'];
 $dbUser = $_ENV['DB_USER'];
 $dbPass = $_ENV['DB_PASSWORD'];
+$dbport= $_ENV['DB_PORT'];
 
-    $conexion = mysqli_connect($dbHost, $dbUser,$dbPass, $dbName);
+    $conexion = mysqli_connect($dbHost, $dbUser,$dbPass, $dbName,$dbport);
     $tildes = $conexion->query("SET NAMES 'utf8'");
     if(mysqli_connect_error()){
         die('No se puede conectar a la base de datos'.mysqli_connect_error());
